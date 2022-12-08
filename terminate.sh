fc3eef40ed1b4dabe4abed031083a929d0efbce0
David <dgleason1990@gmail.com>
2022-12-08 10:11:16 -0500

    removing docker images

--- terminate.sh
+++ terminate.sh
@@ -1,1 +1,1 @@
-docker-compose down && docker volume rm examples_folder_postgres_data   
+docker-compose down && docker volume rm examples_folder_postgres_data && docker rmi examples_folder-api && docker rmi examples_folder-web && docker rmi postgres:13
